ea84985477c260c6d0fadc638b7f9a622ca68e19
sours3s <141668749+sours3s@users.noreply.github.com>
2026-01-09 19:33:49 +0200

    Update launch_game.bat for improved clarity and functionality

--- launch_game.bat
+++ launch_game.bat
@@ -4,12 +4,13 @@ set "LIBS=%ROOT%\libraries"
 set "VERSIONS=%ROOT%\versions"
 set "ARG_FILE=%ROOT%\classpath.args"
 
+:: Создаем файл аргументов с кавычками для каждого JAR
 echo -cp > "%ARG_FILE%"
-:: Цикл с добавлением кавычек для каждого пути
 for /r "%LIBS%" %%i in (*.jar) do (<nul set /p ="^"%%i^";" >> "%ARG_FILE%")
 for /r "%VERSIONS%" %%f in (*.jar) do (<nul set /p ="^"%%f^";" >> "%ARG_FILE%")
 <nul set /p ="^"%ROOT%\mods\highlight.jar^"" >> "%ARG_FILE%"
 
-echo Launching Highlight...
+echo Launching Highlight Client...
+:: Запуск через @-файл
 java -Xmx2G @%ARG_FILE% net.fabricmc.loader.impl.launch.knot.KnotClient --username HighlightUser --version 1.21.4 --gameDir "%ROOT%" --assetsDir "%ROOT%\assets" --assetIndex 1.21.4 --uuid 0 --accessToken 0
 pause
