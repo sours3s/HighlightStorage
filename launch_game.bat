012b4dadddb595eef2dc2f286dfc7a1f771846be
sours3s <141668749+sours3s@users.noreply.github.com>
2026-01-09 19:03:29 +0200

    Update launch_game.bat

--- launch_game.bat
+++ launch_game.bat
@@ -1,11 +1,33 @@
 @echo off
 setlocal enabledelayedexpansion
+
+:: 1. ПУТИ
 set "ROOT=C:\Highlight\minecraft"
 set "LIBS=%ROOT%\libraries"
 set "VERSIONS=%ROOT%\versions"
-set "CP="
-for /r "%LIBS%" %%i in (*.jar) do (set "CP=!CP!%%i;")
-for /r "%VERSIONS%" %%f in (*.jar) do (set "GAME_JAR=%%f")
+set "ARG_FILE=%ROOT%\classpath.args"
+
+:: 2. СОЗДАЁМ ФАЙЛ АРГУМЕНТОВ (Чтобы обойти лимит длины строки)
+echo -cp > "%ARG_FILE%"
+
+:: Собираем все библиотеки в файл по одной
+set "TEMP_CP="
+for /r "%LIBS%" %%i in (*.jar) do (
+    set "TEMP_CP=!TEMP_CP!%%i;"
+    :: Каждые 1000 символов сбрасываем в файл, чтобы не переполнить переменную в батнике
+    if len !TEMP_CP! GTR 1000 (
+        <nul set /p ="!TEMP_CP!" >> "%ARG_FILE%"
+        set "TEMP_CP="
+    )
+)
+
+:: Добавляем версию игры и твой чит
+for /r "%VERSIONS%" %%f in (*.jar) do (set "TEMP_CP=!TEMP_CP!%%f;")
+set "TEMP_CP=!TEMP_CP!%ROOT%\mods\highlight.jar"
+<nul set /p ="!TEMP_CP!" >> "%ARG_FILE%"
+
+:: 3. ЗАПУСК ЧЕРЕЗ @-ФАЙЛ
 echo Launching Highlight...
-java -Xmx2G -cp "%CP%%GAME_JAR%;%ROOT%\mods\highlight.jar" net.fabricmc.loader.impl.launch.knot.KnotClient --username HighlightUser --version 1.21.4 --gameDir "%ROOT%" --assetsDir "%ROOT%\assets" --assetIndex 1.21.4 --uuid 0 --accessToken 0
+java -Xmx2G @%ARG_FILE% net.fabricmc.loader.impl.launch.knot.KnotClient --username HighlightUser --version 1.21.4 --gameDir "%ROOT%" --assetsDir "%ROOT%\assets" --assetIndex 1.21.4 --uuid 0 --accessToken 0
+
 pause
