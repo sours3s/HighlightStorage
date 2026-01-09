04b0894fa5a262d2cfb91c74497dba934fd02e2f
sours3s <141668749+sours3s@users.noreply.github.com>
2026-01-09 18:53:25 +0200

    Update launch_game.bat

--- launch_game.bat
+++ launch_game.bat
@@ -1,29 +1,27 @@
 @echo off
-:: Устанавливаем кодировку UTF-8 для читаемых логов
-chcp 65001 >nul
 setlocal enabledelayedexpansion
 
-:: 1. ПУТИ
+:: 1. SET PATHS
 set "ROOT=C:\Highlight\minecraft"
 set "LIBS=%ROOT%\libraries"
 set "VERSIONS=%ROOT%\versions"
 
-:: 2. СОБИРАЕМ ВСЕ БИБЛИОТЕКИ (Включая твой ASM 9.8)
+:: 2. BUILD CLASSPATH (Loop through all jars)
 set "FULL_CP="
 for /r "%LIBS%" %%i in (*.jar) do (
     set "FULL_CP=!FULL_CP!%%i;"
 )
 
-:: 3. НАХОДИМ ФАЙЛ ИГРЫ В VERSIONS
-for /r "%VERSIONS%" %%f in (*.jar) do set "GAME_JAR=%%f"
+:: 3. FIND GAME JAR
+for /r "%VERSIONS%" %%f in (*.jar) do (
+    set "GAME_JAR=%%f"
+)
 
-:: 4. ЗАПУСК КЛИЕНТА
-echo [Highlight] Запуск игры...
+:: 4. START GAME
+echo Starting Highlight...
 java -Xmx2G -cp "%FULL_CP%%GAME_JAR%;%ROOT%\mods\highlight.jar" net.fabricmc.loader.impl.launch.knot.KnotClient --username HighlightUser --version 1.21.4 --gameDir "%ROOT%" --assetsDir "%ROOT%\assets" --assetIndex 1.21.4 --uuid 0 --accessToken 0
 
 if %ERRORLEVEL% NEQ 0 (
-    echo.
-    echo [!] Произошла ошибка при запуске. 
-    echo [!] Убедитесь, что у вас установлена Java 21.
+    echo Launch failed.
     pause
 )
