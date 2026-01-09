f7a5a95485d7a1452fe1cb37f87bda848f77f20a
sours3s <141668749+sours3s@users.noreply.github.com>
2026-01-09 18:32:20 +0200

    Simplify launch_game.bat by removing unused code
    Removed version detection and error handling for Java.

--- launch_game.bat
+++ launch_game.bat
@@ -1,16 +1,9 @@
 @echo off
 set "ROOT=C:\Highlight\minecraft"
 set "LIBS=%ROOT%\libraries"
-set "VERSIONS=%ROOT%\versions"
+:: Укажи название своей папки из versions
+set "GAME_JAR=%ROOT%\versions\1.21.4-fabric\1.21.4-fabric.jar"
 
-:: Находим первый попавшийся JAR в папке versions (твою версию фабрик)
-for /r "%VERSIONS%" %%f in (*.jar) do set "GAME_JAR=%%f"
-
-:: Команда запуска
-:: Чит подгружается из папки mods
+:: Запуск игры
 java -Xmx2G -cp "%GAME_JAR%;%LIBS%\*;%ROOT%\mods\highlight.jar" net.fabricmc.loader.impl.launch.knot.KnotClient --username HighlightUser --version 1.21.4 --gameDir "%ROOT%" --assetsDir "%ROOT%\assets" --assetIndex 1.21.4 --uuid 0 --accessToken 0
-
-if %ERRORLEVEL% NEQ 0 (
-    echo Ошибка! Проверьте, установлена ли Java 21.
-    pause
-)
+pause
