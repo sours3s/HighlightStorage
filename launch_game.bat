7820c3d3994f76bb6a7703928123928d23c8b49f
sours3s <141668749+sours3s@users.noreply.github.com>
2026-01-09 18:50:35 +0200

    Update launch_game.bat

--- launch_game.bat
+++ launch_game.bat
@@ -1,4 +1,6 @@
 @echo off
+:: Устанавливаем кодировку UTF-8 для читаемых логов
+chcp 65001 >nul
 setlocal enabledelayedexpansion
 
 :: 1. ПУТИ
@@ -6,21 +8,22 @@ set "ROOT=C:\Highlight\minecraft"
 set "LIBS=%ROOT%\libraries"
 set "VERSIONS=%ROOT%\versions"
 
-:: 2. СОБИРАЕМ ВСЕ БИБЛИОТЕКИ В ОДНУ СТРОКУ (Classpath)
-:: Этот цикл проходит по всем подпапкам и добавляет каждый JAR через точку с запятой
+:: 2. СОБИРАЕМ ВСЕ БИБЛИОТЕКИ (Включая твой ASM 9.8)
 set "FULL_CP="
 for /r "%LIBS%" %%i in (*.jar) do (
-    set "FULL_CP=!FULL_CP!;%%i"
+    set "FULL_CP=!FULL_CP!%%i;"
 )
 
-:: 3. НАХОДИМ JAR ИГРЫ
+:: 3. НАХОДИМ ФАЙЛ ИГРЫ В VERSIONS
 for /r "%VERSIONS%" %%f in (*.jar) do set "GAME_JAR=%%f"
 
-:: 4. ЗАПУСК
-:: Теперь в -cp мы передаем ВСЕ найденные библиотеки (!FULL_CP!)
-java -Xmx2G -cp "!FULL_CP!;%GAME_JAR%;%ROOT%\mods\highlight.jar" net.fabricmc.loader.impl.launch.knot.KnotClient --username HighlightUser --version 1.21.4 --gameDir "%ROOT%" --assetsDir "%ROOT%\assets" --assetIndex 1.21.4 --uuid 0 --accessToken 0
+:: 4. ЗАПУСК КЛИЕНТА
+echo [Highlight] Запуск игры...
+java -Xmx2G -cp "%FULL_CP%%GAME_JAR%;%ROOT%\mods\highlight.jar" net.fabricmc.loader.impl.launch.knot.KnotClient --username HighlightUser --version 1.21.4 --gameDir "%ROOT%" --assetsDir "%ROOT%\assets" --assetIndex 1.21.4 --uuid 0 --accessToken 0
 
 if %ERRORLEVEL% NEQ 0 (
-    echo Ошибка запуска! Проверьте консоль.
+    echo.
+    echo [!] Произошла ошибка при запуске. 
+    echo [!] Убедитесь, что у вас установлена Java 21.
     pause
 )
