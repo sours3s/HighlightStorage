2c968e2b7d3bc5c53b988cabe30906441fd76edc
sours3s <141668749+sours3s@users.noreply.github.com>
2026-01-09 19:23:52 +0200

    Update launch_game.bat for clarity and Java 21 support

--- launch_game.bat
+++ launch_game.bat
@@ -4,13 +4,13 @@ set "LIBS=%ROOT%\libraries"
 set "VERSIONS=%ROOT%\versions"
 set "ARG_FILE=%ROOT%\classpath.args"
 
-:: Создаем файл аргументов и берем каждый JAR-путь в кавычки
+:: Создаем файл аргументов и берем КАЖДЫЙ путь в кавычки
 echo -cp > "%ARG_FILE%"
 for /r "%LIBS%" %%i in (*.jar) do (<nul set /p ="^"%%i^";" >> "%ARG_FILE%")
 for /r "%VERSIONS%" %%f in (*.jar) do (<nul set /p ="^"%%f^";" >> "%ARG_FILE%")
 <nul set /p ="^"%ROOT%\mods\highlight.jar^"" >> "%ARG_FILE%"
 
 echo Launching Highlight...
-:: Запускаем игру, используя кавычки для путей с пробелами
+:: Запуск через @-файл с поддержкой Java 21
 java -Xmx2G @%ARG_FILE% net.fabricmc.loader.impl.launch.knot.KnotClient --username HighlightUser --version 1.21.4 --gameDir "%ROOT%" --assetsDir "%ROOT%\assets" --assetIndex 1.21.4 --uuid 0 --accessToken 0
 pause
