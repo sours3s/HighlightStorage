43a20cb2817a11d4b1526400b55c85367b40d7e2
sours3s <141668749+sours3s@users.noreply.github.com>
2026-01-09 18:59:25 +0200

    Update launch_game.bat

--- launch_game.bat
+++ launch_game.bat
@@ -6,6 +6,6 @@ set "VERSIONS=%ROOT%\versions"
 set "CP="
 for /r "%LIBS%" %%i in (*.jar) do (set "CP=!CP!%%i;")
 for /r "%VERSIONS%" %%f in (*.jar) do (set "GAME_JAR=%%f")
-echo Launching...
+echo Launching Highlight...
 java -Xmx2G -cp "%CP%%GAME_JAR%;%ROOT%\mods\highlight.jar" net.fabricmc.loader.impl.launch.knot.KnotClient --username HighlightUser --version 1.21.4 --gameDir "%ROOT%" --assetsDir "%ROOT%\assets" --assetIndex 1.21.4 --uuid 0 --accessToken 0
 pause
