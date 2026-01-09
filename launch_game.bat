d4750a702c92df992e6019c62383797e841180c8
sours3s <141668749+sours3s@users.noreply.github.com>
2026-01-09 18:44:19 +0200

    Update launch_game.bat

--- launch_game.bat
+++ launch_game.bat
@@ -3,17 +3,17 @@ set "ROOT=C:\Highlight\minecraft"
 set "VERSIONS=%ROOT%\versions"
 set "LIBS=%ROOT%\libraries"
 
-:: 1. Находим основной JAR игры (версию Fabric) в папке versions
+:: 1. Находим основной JAR игры в папке versions
 for /r "%VERSIONS%" %%f in (*.jar) do set "GAME_JAR=%%f"
 
-:: 2. Находим JAR загрузчика Fabric в библиотеках (именно он содержит KnotClient)
+:: 2. Находим JAR загрузчика Fabric в библиотеках ( KnotClient находится в нем)
 for /r "%LIBS%" %%f in (*fabric-loader*.jar) do set "LOADER_JAR=%%f"
 
-:: 3. Запуск игры с правильным Classpath (включаем загрузчик, игру и твой чит)
-:: Мы используем -cp чтобы объединить пути к Loader, самой игре и моду Highlight
+:: 3. Запуск игры с объединением путей
+:: Мы добавляем LOADER_JAR, GAME_JAR и твой чит highlight.jar в один classpath
 java -Xmx2G -cp "%LOADER_JAR%;%GAME_JAR%;%ROOT%\mods\highlight.jar" net.fabricmc.loader.impl.launch.knot.KnotClient --username HighlightUser --version 1.21.4 --gameDir "%ROOT%" --assetsDir "%ROOT%\assets" --assetIndex 1.21.4 --uuid 0 --accessToken 0
 
 if %ERRORLEVEL% NEQ 0 (
-    echo Ошибка запуска! Проверьте, что папки libraries и versions скачались полностью.
+    echo Ошибка запуска! Проверьте, что все библиотеки скачались в C:\Highlight\minecraft
     pause
 )
