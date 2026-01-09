e6718379f459187780797417e4b71bd26620d6f2
sours3s <141668749+sours3s@users.noreply.github.com>
2026-01-09 18:47:16 +0200

    Update launch_game.bat

--- launch_game.bat
+++ launch_game.bat
@@ -1,19 +1,26 @@
 @echo off
-set "ROOT=C:\Highlight\minecraft"
-set "VERSIONS=%ROOT%\versions"
-set "LIBS=%ROOT%\libraries"
+setlocal enabledelayedexpansion
 
-:: 1. Находим основной JAR игры в папке versions
+:: 1. ПУТИ
+set "ROOT=C:\Highlight\minecraft"
+set "LIBS=%ROOT%\libraries"
+set "VERSIONS=%ROOT%\versions"
+
+:: 2. СОБИРАЕМ ВСЕ БИБЛИОТЕКИ В ОДНУ СТРОКУ (Classpath)
+:: Этот цикл проходит по всем подпапкам и добавляет каждый JAR через точку с запятой
+set "FULL_CP="
+for /r "%LIBS%" %%i in (*.jar) do (
+    set "FULL_CP=!FULL_CP!;%%i"
+)
+
+:: 3. НАХОДИМ JAR ИГРЫ
 for /r "%VERSIONS%" %%f in (*.jar) do set "GAME_JAR=%%f"
 
-:: 2. Находим JAR загрузчика Fabric в библиотеках ( KnotClient находится в нем)
-for /r "%LIBS%" %%f in (*fabric-loader*.jar) do set "LOADER_JAR=%%f"
-
-:: 3. Запуск игры с объединением путей
-:: Мы добавляем LOADER_JAR, GAME_JAR и твой чит highlight.jar в один classpath
-java -Xmx2G -cp "%LOADER_JAR%;%GAME_JAR%;%ROOT%\mods\highlight.jar" net.fabricmc.loader.impl.launch.knot.KnotClient --username HighlightUser --version 1.21.4 --gameDir "%ROOT%" --assetsDir "%ROOT%\assets" --assetIndex 1.21.4 --uuid 0 --accessToken 0
+:: 4. ЗАПУСК
+:: Теперь в -cp мы передаем ВСЕ найденные библиотеки (!FULL_CP!)
+java -Xmx2G -cp "!FULL_CP!;%GAME_JAR%;%ROOT%\mods\highlight.jar" net.fabricmc.loader.impl.launch.knot.KnotClient --username HighlightUser --version 1.21.4 --gameDir "%ROOT%" --assetsDir "%ROOT%\assets" --assetIndex 1.21.4 --uuid 0 --accessToken 0
 
 if %ERRORLEVEL% NEQ 0 (
-    echo Ошибка запуска! Проверьте, что все библиотеки скачались в C:\Highlight\minecraft
+    echo Ошибка запуска! Проверьте консоль.
     pause
 )
